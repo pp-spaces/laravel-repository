431e38d20f7d0deb88d7c954e2e928db6b45edc7
Socheat <socheatsok78@gmail.com>
2019-03-25 22:10:37 +0700

    Create EloquentRepository Contracts

---
+++ src/Contracts/EloquentRepository.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace PPSpaces\Contracts;
+
+interface EloquentRepository
+{
+
+    /**
+     * Get all of the models from the database.
+     *
+     * @param  array|mixed  $columns
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function all($columns = ['*']);
+
+    /**
+     * Dynamically retrieve attributes on the model.
+     *
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+
+    public function get($columns);
+
+    /**
+     * Get a new query of one model by their IDs.
+     *
+     * @param  array|int  $ids
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function find($id);
+
+    /**
+     * Perform a model create operation.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $query
+     * @return bool
+     */
+    public function create(array $attributes);
+
+    /**
+     * Update the model in the database.
+     *
+     * @param  array  $attributes
+     * @param  array  $options
+     * @return bool
+     */
+    public function update(array $attributes = []);
+
+    /**
+     * Delete the model from the database.
+     *
+     * @return bool|null
+     *
+     * @throws \Exception
+     */
+    public function delete($id);
+
+    /**
+     * Get the value of the model's route key.
+     *
+     * @return mixed
+     */
+    public function getRouteKey();
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName();
+
+    /**
+     * Retrieve the model for a bound value.
+     *
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function resolveRouteBinding($value);
+
+    /**
+     * Convert the object into something JSON serializable.
+     *
+     * @return array
+     */
+    public function jsonSerialize();
+
+    /**
+     * Convert the model to its string representation.
+     *
+     * @return string
+     */
+    public function __toString();
+}
