15ce467be53e8584f27dd4b8f4cc4fad67f9a885
Socheat <socheatsok78@gmail.com>
2019-04-18 22:16:41 +0700

    Update Repository Facade

--- src/Facade/Repository.php
+++ src/Facade/Repository.php
@@ -4,14 +4,18 @@ namespace PPSpaces\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
-class Repository extends Facade {
-
+/**
+ * @see \PPSpaces\Repositories\Repository
+ */
+class Repository extends Facade
+{
     /**
      * Get the registered name of the component.
      *
      * @return string
      */
-    protected static function getFacadeAccessor() { 
-        return 'PPSpaces/Repositories/Repository'; 
+    protected static function getFacadeAccessor()
+    {
+        return 'PPSpaces\Repositories\Repository';
     }
 }
